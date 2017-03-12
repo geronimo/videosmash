d8ea5b9d3f0798ade2fbe1a7895b93450c106f62
Geronimo De Abreu <geronimodeabreu@gmail.com>
2017-03-12 09:31:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videosmash::Application.config.secret_token = 'ca18d724f6133934aac0d82c2e98909af2aba641e78d5b9c68bbd75208c447e0db42ac21374dde55fb46914689ec9b4cc40be552766c78931a3ac5c8210d7e80'
